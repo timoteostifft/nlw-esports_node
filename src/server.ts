4858d97baea7a6e6b93d6f5b6a53e3b976d04ce2
Timóteo Stifft <timoteostifft@gmail.com>
2022-09-22 00:44:18 -0300

    feat: create list games route

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,25 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
 
 const app = express()
-
-app.get('/games', (request, response) => {
-  return response.json([])
+const prisma = new PrismaClient({
+  log: ['query']
 })
 
-app.post('/ads', (request, response) => {
-  return response.status(201).json([])
+app.get('/games', async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true
+        }
+      }
+    }
+  })
+
+  return response.json(games)
 })
 
-app.get('/games/:id/ads', (request, response) => {
-  return response.json([])
-})
 
-app.get('ads/:id/discord', (request, response) => {
-  return response.json([])
-})
 
 app.listen(3333)
